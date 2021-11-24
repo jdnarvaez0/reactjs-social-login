b3f4bd1e8b9705487cfee303b1621404566a7289
navidev0 <juand.ns07@gmail.com>
2021-11-24 18:19:21 -0300

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">Juand</span>
+      <ul className="list">
+        <li className="listItem">
+          <img src="" alt="" className="avatar" />
+        </li>
+        <li className="listItem">Juan David</li>
+        <li className="listItem">Logout</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
